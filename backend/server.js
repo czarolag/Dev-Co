0fe780fbe52725213161d7574ec9bf860a972506
czarolag <czarolag@gmail.com>
2025-03-12 21:36:26 -0700

    Connected MongoDB to backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose")
+const cors = require("cors");
+
 const app = express();
-const port = process.env.PORT || 5000;
+
+
+// Middleware
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+app.use(cors())
+app.use(express.json()); // Parse JSON data
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("MongoDB Connected"))
+        .catch((err) => console.log("Error connecting to MongoDB:", err));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("Hello World!")
   })
   
+
+  // Start Server
+  const port = process.env.PORT || 5000;
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server running on port ${port}`);
   })
