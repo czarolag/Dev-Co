25ad786b40a170691d2961c1ec5afe1e4075d122
czarolag <czarolag@gmail.com>
2025-03-15 00:51:01 -0700

    Connected MongoDB to backend

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,18 @@ app.use(cors())
 app.use(express.json()); // Parse JSON data
 
 // connect to MongoDB
+// https://www.mongodb.com/developer/languages/javascript/getting-started-with-mongodb-and-mongoose/#environment-setup
 mongoose.connect(process.env.MONGO_URI)
         .then(() => console.log("MongoDB Connected"))
         .catch((err) => console.log("Error connecting to MongoDB:", err));
 
-
 app.get('/', (req, res) => {
     res.send("Hello World!")
   })
-  
 
-  // Start Server
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  })
+
+// Start Server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
