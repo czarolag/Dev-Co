8dd9cb921d8a3ab122e3b48424249d6698366be6
czarolag <czarolag@gmail.com>
2025-03-25 18:04:43 -0700

    modified routes

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,15 @@ const cors = require("cors");
 
 const app = express();
 
+const userRoutes = require("./routes/userRoutes");
+
 
 // Middleware
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-app.use(cors())
+app.use(cors({
+  credentials: true,
+}));
+
 app.use(express.json()); // Parse JSON data
 
 // connect to MongoDB
@@ -17,13 +22,13 @@ mongoose.connect(process.env.MONGO_URI)
         .then(() => console.log("MongoDB Connected"))
         .catch((err) => console.log("Error connecting to MongoDB:", err));
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-  })
+
+// Routes        
+app.use("/api/users", userRoutes);
 
 
 // Start Server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-})
+});
