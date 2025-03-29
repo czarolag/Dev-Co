8b98e60ba05d898ecf938573d136f419c75b1825
czarolag <czarolag@gmail.com>
2025-03-29 14:01:43 -0700

    Added login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,9 @@ const User = require("../models/User");
 
 // Signup function
 const signup = async (req, res) => {
-    const { username, email, password } = req.body;
-
     try {
+        const { username, email, password } = req.body;
+
         // check if user exists already
         const existingUser = await User.findOne({ $or: [{ username }, { email }] });
         if (existingUser) {
@@ -21,11 +21,48 @@ const signup = async (req, res) => {
         const user = new User({ username, email, password });
         await user.save();
         const token = user.generateJWT();
-        res.status(201).json({ token });
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false,
+        });
+        
+        res.status(201).json({ message: "User logged in successfully", success: true, user });
     } catch (e) {
         console.error("Error in Signup", e);
         res.status(500).json({ message: "Error registering user" });
     }
 };
 
-module.exports = { signup };
+
+// Login function
+const login = async (req, res) => {
+    try {
+        const { identifier, password } = req.body;
+
+        // find user by username or email
+        const user = await User.findOne({ $or: [{ username: identifier }, { email: identifier }] });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials. User not found." });
+        }
+
+        // validate password
+        const isMatch = await user.isValidPassword(password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials. Wrong password." });
+        }
+
+        const token = user.generateJWT();
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false,
+        });
+
+        res.status(200).json({ message: "User logged in successfully", success: true, user });
+    } catch (e) {
+        console.error("Error in Login", e);
+        res.status(500).json({ message: "Error logging in user" });
+    }
+}
+
+module.exports = { signup, login };
