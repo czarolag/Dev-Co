307ce9071c1baf67e55fa26de079479575d60442
czarolag <czarolag@gmail.com>
2025-04-05 20:02:05 -0700

    added backend origin for cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: process.env.VITE_API_URL || "http://localhost:5000",
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
+});
