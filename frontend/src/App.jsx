824da4c1cd6376f7d9c686cbd8100a8b6be2939d
czarolag <czarolag@gmail.com>
2025-04-05 20:09:57 -0700

    Set up base routing for App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppTheme from './shared-theme/AppTheme';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+import { UserContextProvider } from './context/userContext';
+import axios from "axios";
+import CssBaseline from "@mui/material/CssBaseline";
+import ColorModeSelect from './shared-theme/ColorModeSelect';
 
-function App() {
+import SignUp from "./pages/SignUp";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import "./App.css";
+
+// set up axios
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+axios.defaults.withCredentials = true;
+
+
+function App(props) {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <ColorModeSelect sx={{ position: 'fixed', bottom: '1rem', right: '1rem' }} />
+      <UserContextProvider>
+        <Router>
+          <header>
+            <NavBar />
+            <Toaster position='bottom-left' toastOptions={{ duration: 3000 }} />
+          </header>
+          <main>
+            <Routes>
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </main>
+        </Router>
+      </UserContextProvider>
+    </AppTheme>
   )
-}
+};
 
-export default App
+export default App;
