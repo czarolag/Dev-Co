59260aa31fee23bcd4a8937725e5d644dbc6938b
czarolag <czarolag@gmail.com>
2025-04-05 20:12:02 -0700

    Added mock Home page for testing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div style={{ padding: "200px" }}>
+            <h1 style={{ display: "block" }}>Temp Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
