06dad98c472f696c1e23903298c5d3871085793a
czarolag <czarolag@gmail.com>
2025-04-05 20:03:04 -0700

    added signing in user on successful signup

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/userContext";
+
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
@@ -11,10 +15,6 @@ import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import MuiCard from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
-import AppTheme from '../shared-theme/AppTheme';
-import ColorModeSelect from '../shared-theme/ColorModeSelect';
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 
 // template from :https://mui.com/material-ui/getting-started/templates/
@@ -27,38 +27,38 @@ const Card = styled(MuiCard)(({ theme }) => ({
     gap: theme.spacing(2),
     margin: 'auto',
     boxShadow:
-      'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
     [theme.breakpoints.up('sm')]: {
-      width: '450px',
+        width: '450px',
     },
     ...theme.applyStyles('dark', {
-      boxShadow:
-        'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+        boxShadow:
+            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
     }),
-  }));
-  
-  const SignUpContainer = styled(Stack)(({ theme }) => ({
+}));
+
+const SignUpContainer = styled(Stack)(({ theme }) => ({
     height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
     minHeight: '100%',
     padding: theme.spacing(2),
     [theme.breakpoints.up('sm')]: {
-      padding: theme.spacing(4),
+        padding: theme.spacing(4),
     },
     '&::before': {
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      zIndex: -1,
-      inset: 0,
-      backgroundImage:
-        'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-      backgroundRepeat: 'no-repeat',
-      ...theme.applyStyles('dark', {
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        zIndex: -1,
+        inset: 0,
         backgroundImage:
-          'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-      }),
+            'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+        backgroundRepeat: 'no-repeat',
+        ...theme.applyStyles('dark', {
+            backgroundImage:
+                'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+        }),
     },
-  }));
+}));
 
 
 function SignUp(props) {
@@ -68,6 +68,9 @@ function SignUp(props) {
     const [emailErrorMessage, setEmailErrorMessage] = useState("");
     const [passwordError, setPasswordError] = useState(false);
     const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+
+    const navigate = useNavigate();
+    const { setUser } = useContext(UserContext);
 
     const validateInputs = () => {
         const username = document.getElementById("username");
@@ -120,109 +123,99 @@ function SignUp(props) {
             password: document.getElementById("password").value,
         };
 
-        try {
-            const response = await fetch(`${API_URL}/api/users/signup`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data),
+        // https://permify.co/post/jwt-authentication-in-react/
+        await axios.post("/api/users/signup", data)
+            .then(res => {
+                toast.success("Successfully created user.")
+                setUser(res.data.user);
+                navigate("/");
+            })
+            .catch(e => {
+                console.log(e)
+                toast.error(e.response.data.message);
             });
-
-            const result = await response.json();
-
-            if (!response.ok) {
-                throw new Error(result.message || "Signup failed.");
-            }
-
-            alert("User registered successfully!");
-        } catch (error) {
-            alert(error.message);
-        }
     };
 
 
     return (
-        <AppTheme {...props}>
-            <CssBaseline enableColorScheme />
-            <ColorModeSelect sx={{ position: 'fixed', bottom: '1rem', right: '1rem' }}/>
-            <SignUpContainer direction="column" justifyContent="space-between">
-                <Card variant="outlined">
-                    <Typography component="h1" variant="h4"
-                        sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
-                    >
+        <SignUpContainer direction="column" justifyContent="space-between">
+            <Card variant="outlined">
+                <Typography component="h1" variant="h4"
+                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+                >
+                    Sign up
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="name">Username</FormLabel>
+                        <TextField
+                            autoComplete="username"
+                            name="username"
+                            required
+                            fullWidth
+                            id="username"
+                            placeholder="username"
+                            error={usernameError}
+                            helperText={usernameErrorMessage}
+                            color={usernameError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            id="email"
+                            placeholder="your@email.com"
+                            name="email"
+                            autoComplete="email"
+                            variant="outlined"
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            color={passwordError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            name="password"
+                            placeholder="******"
+                            type="password"
+                            id="password"
+                            autoComplete="new-password"
+                            variant="outlined"
+                            error={passwordError}
+                            helperText={passwordErrorMessage}
+                            color={passwordError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>
                         Sign up
+                    </Button>
+                </Box>
+                <Divider>
+                    <Typography sx={{ color: 'text.secondary' }}>or</Typography>
+                </Divider>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <Typography sx={{ textAlign: 'center' }}>
+                        Already have an account?{' '}
+                        <Link
+                            href="/signin"
+                            variant="body2"
+                            sx={{ alignSelf: 'center' }}
+                        >
+                            Sign in
+                        </Link>
                     </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
-                    >
-                        <FormControl>
-                            <FormLabel htmlFor="name">Username</FormLabel>
-                            <TextField
-                                autoComplete="username"
-                                name="username"
-                                required
-                                fullWidth
-                                id="username"
-                                placeholder="username"
-                                error={usernameError}
-                                helperText={usernameErrorMessage}
-                                color={usernameError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="email">Email</FormLabel>
-                            <TextField
-                                required
-                                fullWidth
-                                id="email"
-                                placeholder="your@email.com"
-                                name="email"
-                                autoComplete="email"
-                                variant="outlined"
-                                error={emailError}
-                                helperText={emailErrorMessage}
-                                color={passwordError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="password">Password</FormLabel>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                placeholder="******"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                variant="outlined"
-                                error={passwordError}
-                                helperText={passwordErrorMessage}
-                                color={passwordError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-                        <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>
-                            Sign up
-                        </Button>
-                    </Box>
-                    <Divider>
-                        <Typography sx={{ color: 'text.secondary' }}>or</Typography>
-                    </Divider>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                        <Typography sx={{ textAlign: 'center' }}>
-                            Already have an account?{' '}
-                            <Link
-                                href="/login-route"
-                                variant="body2"
-                                sx={{ alignSelf: 'center' }}
-                            >
-                                Sign in
-                            </Link>
-                        </Typography>
-                    </Box>
-                </Card>
-            </SignUpContainer>
-        </AppTheme>
+                </Box>
+            </Card>
+        </SignUpContainer>
     );
 }
 
